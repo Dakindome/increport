4a1573a0a5f0c0b51e441607fb333f757dc44814
Dakin <dakin.dome@gmail.com>
2016-12-26 17:30:27 +0700

    Add files via upload

---
+++ GridviewToExcel/GridviewToExcel/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace GridviewToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadGrid = new System.Windows.Forms.Button();
+            this.Ex2Excel = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.datefrom = new System.Windows.Forms.DateTimePicker();
+            this.dateto = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadGrid
+            // 
+            this.LoadGrid.Location = new System.Drawing.Point(317, 19);
+            this.LoadGrid.Name = "LoadGrid";
+            this.LoadGrid.Size = new System.Drawing.Size(113, 23);
+            this.LoadGrid.TabIndex = 0;
+            this.LoadGrid.Text = "LoadData";
+            this.LoadGrid.UseVisualStyleBackColor = true;
+            this.LoadGrid.Click += new System.EventHandler(this.LoadGrid_Click);
+            // 
+            // Ex2Excel
+            // 
+            this.Ex2Excel.Location = new System.Drawing.Point(467, 19);
+            this.Ex2Excel.Name = "Ex2Excel";
+            this.Ex2Excel.Size = new System.Drawing.Size(120, 23);
+            this.Ex2Excel.TabIndex = 1;
+            this.Ex2Excel.Text = "Export To Excel";
+            this.Ex2Excel.UseVisualStyleBackColor = true;
+            this.Ex2Excel.Click += new System.EventHandler(this.Ex2Excel_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1120, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // datefrom
+            // 
+            this.datefrom.Location = new System.Drawing.Point(12, 22);
+            this.datefrom.Name = "datefrom";
+            this.datefrom.Size = new System.Drawing.Size(130, 20);
+            this.datefrom.TabIndex = 3;
+            // 
+            // dateto
+            // 
+            this.dateto.Location = new System.Drawing.Point(166, 22);
+            this.dateto.Name = "dateto";
+            this.dateto.Size = new System.Drawing.Size(132, 20);
+            this.dateto.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Date from :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Date To :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(375, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateto);
+            this.Controls.Add(this.datefrom);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Ex2Excel);
+            this.Controls.Add(this.LoadGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadGrid;
+        private System.Windows.Forms.Button Ex2Excel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker datefrom;
+        private System.Windows.Forms.DateTimePicker dateto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GridviewToExcel/GridviewToExcel/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.Office.Interop.Excel;
+
+
+
+
+
+namespace GridviewToExcel
+{
+    public partial class Form1 : Form
+    {
+        public string MyConnectionStrings = ConfigurationManager.ConnectionStrings["INC_SVH_connection"].ConnectionString;
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadGrid_Click(object sender, EventArgs e)
+        {
+
+            DataSet ds = new DataSet();
+            // System.Data.DataTable dt = new System.Data.DataTable();
+            System.Data.DataTable dt = ds.Tables.Add("Orders");
+            
+            SqlConnection con = new SqlConnection(MyConnectionStrings);
+           
+
+            string sql_inc = @"SELECT [A_DOC_NO],[A_DTE],[A_DTE],[A_DTE_FRM],[A_TME],[A_INC_CAT],[A_INC_CAT_DES],[A_DEP],[A_DEP_DES],[A_INC_TYP],[A_INC_TYP_DES]
+,[A_INC_SUB],[A_INC_SUB_DES],[A_DEP2_DES1],[A_DEP2_DES2],[A_DEP2_DES3],[A_DEP2_DES4],[A_DEP2_DES5],[A_Committee1],[A_Committee2],[A_Committee3]
+,[A_RN],[A_RMK],[A_SVR],[A_SVR_DES],[A_SLV],[A_INC_STS_DES],[A_NTE1],[A_NTE2],[A_NTE3],[A_NTE1],[A_NTE2],[A_NTE3]
+                            FROM INCIDENTANN";
+            SqlDataAdapter da = new SqlDataAdapter(sql_inc, con);
+            da.Fill(ds, "IncedentReport");
+
+            
+            //set column in datatable 
+            dt.Columns.Add("Doc_number", typeof(string));
+            dt.Columns.Add("Month", typeof(string));
+            dt.Columns.Add("Date_key", typeof(DateTime));
+            dt.Columns.Add("Date_Occur", typeof(DateTime));
+            dt.Columns.Add("Time_Occur", typeof(DateTime));
+
+            dt.Columns.Add("CategoryCode", typeof(string));
+            dt.Columns.Add("CategoryDesc", typeof(string));
+            dt.Columns.Add("Dept_key", typeof(string));
+            dt.Columns.Add("Dept_key_Desc", typeof(string));
+            dt.Columns.Add("Type", typeof(string));
+
+            dt.Columns.Add("Type_Desc", typeof(string));
+            dt.Columns.Add("SubType", typeof(string));
+            dt.Columns.Add("SubType_Desc", typeof(string));
+            dt.Columns.Add("Dept_Concern1", typeof(string));
+            dt.Columns.Add("Dept_Concern2", typeof(string));
+
+            dt.Columns.Add("Dept_Concern3", typeof(string));
+            dt.Columns.Add("Dept_Concern4", typeof(string));
+            dt.Columns.Add("Dept_Concern5", typeof(string));
+            dt.Columns.Add("Committee1", typeof(string));
+            dt.Columns.Add("Committee2", typeof(string));
+
+            dt.Columns.Add("Committee3", typeof(string));
+            dt.Columns.Add("HN", typeof(string));
+            dt.Columns.Add("Remark", typeof(string));
+            dt.Columns.Add("Severity", typeof(string));
+            dt.Columns.Add("SeverityDesc", typeof(string));
+
+            dt.Columns.Add("Solve_Primary", typeof(string));
+            dt.Columns.Add("Status", typeof(string));
+            dt.Columns.Add("Response1", typeof(string));
+            dt.Columns.Add("Response2", typeof(string));
+            dt.Columns.Add("Response3", typeof(string));
+
+            dt.Columns.Add("CommentByRM", typeof(string));
+            dt.Columns.Add("RCA_Code", typeof(string));
+            dt.Columns.Add("RCA_Desc", typeof(string));
+
+            
+
+            //dataGridView1.DataSource = dt;
+            dataGridView1.DataSource = ds.Tables["IncedentReport"];
+
+
+            label3.Text = datefrom.Value.ToString("dd-MMM-yyyy");
+            label4.Text = dateto.Value.ToString("dd-MMM-yyyy");
+
+        }
+
+        private void Ex2Excel_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application Excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook wb = Excel.Workbooks.Add(XlSheetType.xlWorksheet);
+            Worksheet ws = (Worksheet)Excel.ActiveSheet;
+            ws.Rows.RowHeight = 15;
+            Excel.Visible = true;
+   
+            ws.Cells[1, 1] = "Doc_number";
+            ws.Cells[1, 2] = "Month";
+            ws.Cells[1, 3] = "Date_key";
+            ws.Cells[1, 4] = "Date_Occur";
+            ws.Cells[1, 5] = "Time_Occur";
+            ws.Cells[1, 6] = "CategoryCode";
+            ws.Cells[1, 7] = "CategoryDesc";
+            ws.Cells[1, 8] = "Dept_key";
+            ws.Cells[1, 9] = "Dept_key_Desc";
+            ws.Cells[1, 10] = "Type";
+            ws.Cells[1, 11] = "Type_Desc";
+            ws.Cells[1, 12] = "SubType";
+            ws.Cells[1, 13] = "SubType_Desc";
+            ws.Cells[1, 14] = "Dept_Concern1";
+            ws.Cells[1, 15] = "Dept_Concern2";
+            ws.Cells[1, 16] = "Dept_Concern3";
+            ws.Cells[1, 17] = "Dept_Concern4";
+            ws.Cells[1, 18] = "Dept_Concern5";
+            ws.Cells[1, 19] = "Committee1";
+            ws.Cells[1, 20] = "Committee2";
+            ws.Cells[1, 21] = "Committee3";
+            ws.Cells[1, 22] = "HN";
+            ws.Cells[1, 23] = "Remark";
+            ws.Cells[1, 24] = "Severity";
+            ws.Cells[1, 25] = "SeverityDesc";
+            ws.Cells[1, 26] = "Solve_Primary";
+            ws.Cells[1, 27] = "Status";
+            ws.Cells[1, 28] = "Response1";
+            ws.Cells[1, 29] = "Response2";
+            ws.Cells[1, 30] = "Response3";
+            ws.Cells[1, 31] = "CommentByRM";
+            ws.Cells[1, 32] = "RCA_Code";
+            ws.Cells[1, 33] = "RCA_Desc";
+            
+
+            for (int j = 2; j <= dataGridView1.Rows.Count; j++)
+            {
+                for (int i = 1; i <= 33; i++)
+                {
+                    ws.Cells[j,i] =dataGridView1.Rows[j-2].Cells[i-1].Value;
+                }
+            }
+
+        }
+    }
+}
